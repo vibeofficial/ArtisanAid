5b5591b2a7dc9400d8706375b6ad004b6e90da76
vibeofficial <ichioguchristopher98@gmail.com>
2025-04-16 18:29:00 +0100

    correction done

--- controllers/employer.js
+++ controllers/employer.js
@@ -86,7 +86,7 @@ exports.registerEmployer = async (req, res) => {
     });
 
     const token = jwt.sign({ id: employer._id }, jwtSecret, { expiresIn: '5mins' });
-    const link = `${req.protocol}://${req.get('host')}/v1/verify/account/${token}`;
+    const link = `${req.protocol}://artisian-aid.vercel.app/verifyemail/${token}`;
     const html = verifyMail(link);
 
     const mailDetails = {
